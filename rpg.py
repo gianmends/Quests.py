cdf9e9c221b04170fb3dd93253a5e26cd1be9495
Giancarlo Mendes <83793178+gianmends@users.noreply.github.com>
2023-03-25 11:15:06 -0300

    Update rpg.py

--- rpg.py
+++ rpg.py
@@ -1,24 +1,24 @@
 class SerVivo:
   def __init__(self, vida, ataque):
-    self.vida = vida
-    self.ataque = ataque
+    self.vida:int = vida
+    self.ataque:int = ataque
 
 class Personagem(SerVivo):
   def __init__(self, vida, ataque, nome):
     super().__init__(vida, ataque)
-    self.nome = nome
+    self.nome:str = nome
 
 class monstro(SerVivo):
   def __init__(self, vida, ataque, tipo):
     super().__init__(vida, ataque)
-    self.tipo = tipo
+    self.tipo:str = tipo
 
 class lobo(monstro):
   def __init__(self, vida, ataque, tipo, força):
     super().__init__(vida, ataque, tipo)
-    self.força = força
+    self.força:int = força
   
 class goblin(monstro):
   def __init__(self, vida, ataque, tipo, inteligencia):
     super().__init__(vida, ataque, tipo)
-    self.interligencia = inteligencia
+    self.interligencia:int = inteligencia
